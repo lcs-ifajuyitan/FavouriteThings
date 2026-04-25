33a65d4fdd285eba07de402e970d97000106d833
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-04-25 10:27:54 -0400

    Initial Commit

---
+++ FavouriteThings/FavouriteThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteThingsApp.swift
+//  FavouriteThings
+//
+//  Created by ☆ on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
